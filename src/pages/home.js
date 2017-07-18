9e000a28d600ef9b057deb8ddf226a49e9310376
hellosylvee <helloimsylvia@gmail.com>
2017-07-17 20:14:28 -0400

    created template for projects on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Header, List, Icon, Image } from 'semantic-ui-react'
+import { Grid, Card, Header, List, Icon, Image } from 'semantic-ui-react'
 
 import Emem from '../projects/Emem'
 import IntroCard from '../components/intro-card'
@@ -15,45 +15,60 @@ class home extends Component {
             <IntroCard/>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row centered columns={3}>
+        <Grid.Row centered columns={4}>
           <Grid.Column>
-            <Image centered bordered src='assets/images/dino.jpg' name='bird' size='medium' />
-            <List horizontal>
-              <List.Item>
-                <List.Content verticalAlign='middle'>
-                  <a target='_blank' rel='noopener noreferrer'
-                    href='https://www.github.com/hellosylvee'>
-                    <Icon link name='github' color='blue' size='big'/>Github
-                  </a>
-                </List.Content>
-              </List.Item>
-            </List>
+            <Card centered>
+              <Image src='assets/images/dino.jpg' name='bird' size='medium' />
+              <Card.Content>
+                <Card.Header>Punny Posts</Card.Header>
+                <Card.Meta>A social website to create and appreciate word puns and gifs</Card.Meta>
+                <Card.Description className='card-desc-align'>Built with React with Rails backend, styled with Semantic CSS framework. Utilized JWT for user authentication and Giphy API for 3rd party content.</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a target='_blank' rel='noopener noreferrer' href='https://www.github.com/hellosylvee/punny-posts'>
+                  <Icon name='github' size='large'/>Github
+                </a>
+                <a className='demo-play' target='_blank' rel='noopener noreferrer' href='https://www.github.com/hellosylvee/'>
+                  <Icon name='video play outline' size='large'/>Demo
+                </a>
+              </Card.Content>
+            </Card>
           </Grid.Column>
           <Grid.Column>
-            <Image centered bordered src='assets/images/dino.jpg' name='bird' size='medium' />
-            <List horizontal>
-              <List.Item>
-                <List.Content verticalAlign='middle'>
-                  <a target='_blank' rel='noopener noreferrer'
-                    href='https://www.github.com/hellosylvee'>
-                    <Icon link name='github' color='blue' size='big'/>Github
-                  </a>
-                </List.Content>
-              </List.Item>
-            </List>
+            <Card centered floated='right'>
+              <Image src='assets/images/dino.jpg' name='bird' size='medium' />
+              <Card.Content>
+                <Card.Header>emeM Meme</Card.Header>
+                <Card.Meta>A meme generator</Card.Meta>
+                <Card.Description className='card-desc-align'>Built in React with Rails backend and styled with Semantic CSS framework. Used Super-Agent and Dropzone for image handling, and Cloudinary for image storage.</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a target='_blank' rel='noopener noreferrer' href='https://www.github.com/hellosylvee/nomnom'>
+                  <Icon name='github' size='large'/>Github
+                </a>
+                <a className='demo-play' target='_blank' rel='noopener noreferrer' href='https://www.github.com/hellosylvee/'>
+                  <Icon name='video play outline' size='large'/>Demo
+                </a>
+              </Card.Content>
+            </Card>
           </Grid.Column>
           <Grid.Column>
-            <Image centered bordered src='assets/images/dino.jpg' name='bird' size='medium' />
-            <List horizontal>
-              <List.Item>
-                <List.Content verticalAlign='middle'>
-                  <a target='_blank' rel='noopener noreferrer'
-                    href='https://www.github.com/hellosylvee'>
-                    <Icon link name='github' color='blue' size='big'/>Github
-                  </a>
-                </List.Content>
-              </List.Item>
-            </List>
+            <Card centered floated='right'>
+              <Image src='assets/images/dino.jpg' name='bird' size='medium' />
+              <Card.Content>
+                <Card.Header>RadLibz</Card.Header>
+                <Card.Meta>An interactive word game</Card.Meta>
+                <Card.Description className='card-desc-align'>A spinoff of a popular word game, Madlibs. A user can create and save stories, and also view their word bank of frequently used words. Built with Ruby on Rails, styled with Materialize CSS framework.</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a target='_blank' rel='noopener noreferrer' href='https://www.github.com/hellosylvee/radlibz'>
+                  <Icon name='github' size='large'/>Github
+                </a>
+                <a className='demo-play' target='_blank' rel='noopener noreferrer' href='https://www.github.com/hellosylvee/'>
+                  <Icon name='video play outline' size='large'/>Demo
+                </a>
+              </Card.Content>
+            </Card>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row centered columns={3}>
--- src/pages/misc.js
+++ src/pages/misc.js
@@ -8,7 +8,7 @@ class process extends Component {
       <Grid>
         <Grid.Row centered columns={3}>
           <Header as='h1' className='misc-title animated fadeInDown'>Photography</Header>
-          <div className='small-text'>Taken with Note 4</div>
+          <div className='small-text'>Taken with Note 4 and Pixel</div>
           <Grid.Column className='misc-photo animated rollIn'>
             <Image centered bordered src='assets/images/dino.jpg' name='dinosaur skeleton' floated='right' size='medium' />
           </Grid.Column>
@@ -27,7 +27,18 @@ class process extends Component {
             <Image centered bordered src='assets/images/bridge.jpg' name='brooklyn bridge' size='medium' />
           </Grid.Column>
           <Grid.Column className='misc-photo animated rollIn'>
-            <Image centered bordered src='assets/images/fire.jpg' name='woman breathing fire' floated='left' size='medium' />
+            <Image centered bordered src='assets/images/love.jpg' name='love sign on water' floated='left' size='medium' />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered columns={3}>
+          <Grid.Column className='misc-photo animated rollIn'>
+            <Image centered bordered src='assets/images/lobsteroll.jpg' name='lobster roll' floated='right' size='medium' />
+          </Grid.Column>
+          <Grid.Column className='misc-photo animated rollIn'>
+            <Image centered bordered src='assets/images/sign.jpg' name='inspirational message' size='medium' />
+          </Grid.Column>
+          <Grid.Column className='misc-photo animated rollIn'>
+            <Image centered bordered src='assets/images/hollywood.jpg' name='hollywood sign' floated='left' size='medium' />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row centered columns={3}>
@@ -35,21 +46,10 @@ class process extends Component {
             <Image centered bordered src='assets/images/donuts.jpg' name='donuts' floated='right' size='medium' />
           </Grid.Column>
           <Grid.Column className='misc-photo animated rollIn'>
-            <Image centered bordered src='assets/images/lookback.jpg' name='street sign' size='medium' />
+            <Image centered bordered src='assets/images/ramen.jpg' name='ramen' size='medium' />
           </Grid.Column>
           <Grid.Column className='misc-photo animated rollIn'>
-            <Image centered bordered src='assets/images/bnw.jpg' name='street in black and white' floated='left' size='medium' />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row centered columns={3}>
-          <Grid.Column className='misc-photo animated rollIn'>
-            <Image centered bordered src='assets/images/rainbow.jpg' name='rainbow' floated='right' size='medium' />
-          </Grid.Column>
-          <Grid.Column className='misc-photo animated rollIn'>
-            <Image centered bordered src='assets/images/sign.jpg' name='inspirational message' size='medium' />
-          </Grid.Column>
-          <Grid.Column className='misc-photo animated rollIn'>
-            <Image centered bordered src='assets/images/soulfood.jpg' name='soul food' floated='left' size='medium' />
+            <Image centered bordered src='assets/images/fire.jpg' name='woman breathing fire' floated='left' size='medium' />
           </Grid.Column>
         </Grid.Row>
       </Grid>
