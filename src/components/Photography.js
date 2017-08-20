b1d717145df9d0d6eb0373c4bc8628f71f799516
hellosylvee <helloimsylvia@gmail.com>
2017-08-20 13:08:37 -0400

    removed extra columns

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -16,87 +16,67 @@ class Photography extends Component {
               src='assets/images/dino.jpg'
               name='dinosaur skeleton'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+
             <Image
               bordered centered
               src='assets/images/chihuly.jpg'
               name='ferris wheel'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+
             <Image
               bordered centered
               src='assets/images/bird.jpg'
               name='bird'
               size='massive' />
-          </Grid.Column>
-        </Grid.Row>
 
-        <Grid.Row>
-          <Grid.Column>
             <Image
               bordered centered
               src='assets/images/postal.jpg'
               name='post office'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+
             <Image
               bordered centered
               src='assets/images/bridge.jpg'
               name='brooklyn bridge'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+
             <Image
               bordered centered
               src='assets/images/love.jpg'
               name='love sign on water'
               size='massive' />
-          </Grid.Column>
-        </Grid.Row>
 
-        <Grid.Row>
-          <Grid.Column>
             <Image
               bordered centered
               src='assets/images/whale.jpg'
               name='lobster roll'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+
             <Image
               bordered centered
               src='assets/images/starbucks.jpg'
               name='inspirational message'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+
             <Image
               bordered centered
               src='assets/images/spice.jpg'
               name='hollywood sign'
               size='massive' />
-          </Grid.Column>
-        </Grid.Row>
 
-        <Grid.Row>
-          <Grid.Column>
             <Image
               bordered centered
               src='assets/images/donuts.jpg'
               name='donuts'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+        
             <Image
               bordered centered
               src='assets/images/ramen.jpg'
               name='ramen'
               size='massive' />
-          </Grid.Column>
-          <Grid.Column>
+        
             <Image
               bordered centered
               src='assets/images/fire.jpg'
